b3a7c2e4a415cce52834ce6b147be31ccd998e3a
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-06-07 11:57:44 -0500

    first commit ejercicio

--- src/segundo/trabajo/PrimerEje.java
+++ src/segundo/trabajo/PrimerEje.java
@@ -5,19 +5,23 @@ import java.util.Scanner;
 public class PrimerEje {
 
     public static void main(String[] args) {
-        Scanner t= new Scanner(System.in);
+        Scanner texto= new Scanner(System.in);
         
         System.out.println("Escriba el nombre");
-        String nombre = t.nextLine();
+        String nombre =  texto.nextLine();
         System.out.println("ingrese edad");
-        int edad = t.nextInt();
+        int edad = texto.nextInt();
         System.out.println("estado civil: ");
         System.out.println("0=soltero");
         System.out.println("1=casado");
-        int civil=t.nextInt();
+        int civil=texto.nextInt();
         
         if(edad>=18 && civil==0){
-            System.out.println(""+nombre+" tiene "+edad+" años y esta "+civil);
+            System.out.println(""+nombre+" tiene "+edad+" años y esta soltero");
+        } else if (edad>=18 && civil==1) {
+            System.out.println(""+nombre+" tiene "+edad+" años y esta casado");
+        }else{
+            System.out.println("es menor de edad o se dijito mal el estado civil");
         }
     }
     
