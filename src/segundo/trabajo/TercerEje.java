1ca3f7cb51f13adfa85371e9c7b2edf280c132bc
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-06-10 20:07:24 -0500

    second commit ejercicio

--- src/segundo/trabajo/TercerEje.java
+++ src/segundo/trabajo/TercerEje.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class TercerEje {
     public static void main(String[] args) {
-        Scanner t=new Scanner(System.in);
+        Scanner textoScanner=new Scanner(System.in);
         
         System.out.println("escriba un numero");
-        int nu=t.nextInt();
+        int numero=textoScanner.nextInt();
         
-        if(nu%2==0){
-            System.out.println(nu+" es par");
+        if(numero%2==0){
+            System.out.println(numero+" es par");
         }else{
-            System.out.println(nu+" no es par");
+            System.out.println(numero+" no es par");
         }
     }
     
