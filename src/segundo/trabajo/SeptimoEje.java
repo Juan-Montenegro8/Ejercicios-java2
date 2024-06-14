74355969b4434bbd6078ac1d17811516c4c00952
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-06-13 19:40:14 -0500

    thirds commit ejercicio

--- src/segundo/trabajo/SeptimoEje.java
+++ src/segundo/trabajo/SeptimoEje.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class SeptimoEje {
 
     public static void main(String[] args) {
-        Scanner t=new Scanner(System.in);
+        Scanner textoScanner=new Scanner(System.in);
         
         System.out.println("ingrese un numero");
-        int n1=t.nextInt();
+        int n1=textoScanner.nextInt();
         System.out.println("ingrese un numero");
-        int n2=t.nextInt();
+        int n2=textoScanner.nextInt();
         
         if(n1>=0 && n2<=0){
             System.out.println("el numero "+n1+" es positivo");
