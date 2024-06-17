1c05905f877a4221a487100c34de9395eff2612b
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-06-17 14:05:00 -0500

    fourth commit ejercicio

--- src/segundo/trabajo/OctavoEje.java
+++ src/segundo/trabajo/OctavoEje.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class OctavoEje {
 
     public static void main(String[] args) {
-        Scanner t=new Scanner(System.in);
+        Scanner textoScanner=new Scanner(System.in);
         
         System.out.println("escriba un caracter");
-        char letra=t.next().charAt(0);
+        char letra=textoScanner.next().charAt(0);
         
         if(Character.isUpperCase(letra)){
             System.out.println("Mayuscula");
