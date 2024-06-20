6b782c7d451d207623495ef58f379ed86eb1c283
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-06-19 19:55:02 -0500

    fifth commit ejercicio

--- src/segundo/trabajo/DecimoEje.java
+++ src/segundo/trabajo/DecimoEje.java
@@ -8,15 +8,15 @@ public class DecimoEje {
         Scanner textoScanner=new Scanner(System.in);
         
         System.out.println("ingrese un numero");
-        int n1=textoScanner.nextInt();
+        int numero1=textoScanner.nextInt();
         System.out.println("ingrese un numero");
-        int n2=textoScanner.nextInt();
+        int numero2=textoScanner.nextInt();
         
-        if(n1<n2){
-            System.out.println(n2+" es mayor que "+n1);
+        if(numero1<numero2){
+            System.out.println(numero2+" es mayor que "+numero1);
         }else{
-            if(n2<n1){
-                System.out.println(n1+" es mayor que "+n2);
+            if(numero2<numero1){
+                System.out.println(numero1+" es mayor que "+numero2);
             }else{
                 System.out.println("los numero son iguales");
             }
