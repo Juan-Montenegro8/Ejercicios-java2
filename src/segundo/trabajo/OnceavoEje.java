32859222240464a26122a89f8be11be522849fd6
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-06-21 19:52:04 -0500

    fifth commit ejer

--- src/segundo/trabajo/OnceavoEje.java
+++ src/segundo/trabajo/OnceavoEje.java
@@ -14,27 +14,27 @@ public class OnceavoEje {
         switch (rectangulo) {
             case 1:                
                 System.out.println("ingrese base");
-                double base=textoScanner.nextDouble();
+                double base1=textoScanner.nextDouble();
                 System.out.println("ingrese altura");
-                double altura=textoScanner.nextDouble();
-                if(base>0 && altura>0){
-                    double area =(base*altura)/2;
-                    System.out.println("el area del triangulo "+area);
+                double altura1=textoScanner.nextDouble();
+                if(base1>0 && altura1>0){
+                    double area1 =(base1*altura1)/2;
+                    System.out.println("el area del triangulo "+area1);
                 }else{
                     System.out.println("no pueden ir numeros negativos o el 0");
                 }
                 break;
             case 2:
                 System.out.println("ingrese base");
-                double b1=textoScanner.nextDouble();
+                double base2=textoScanner.nextDouble();
                 System.out.println("ingrese cateto1");
                 double a1=textoScanner.nextDouble();
                 System.out.println("ingrese cateto2");
                 double c=textoScanner.nextDouble();
                 
-                if(b1>0 && a1>0 && c>0){
-                    double s=(a1+b1+c)/2;
-                    double rai=(s*(s-a1)*(s-b1)*(s-c));
+                if(base2>0 && a1>0 && c>0){
+                    double s=(a1+base2+c)/2;
+                    double rai=(s*(s-a1)*(s-base2)*(s-c));
                     double area1 = Math.sqrt(rai);
                 
                     System.out.println("el area del triangulo "+area1);
