bb4537807ac54e28bf18bd8e0bfcf281f3e94ff7
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-03 20:24:57 -0500

    Firt commit scanner

--- src/segundo/trabajo/SegundoEje.java
+++ src/segundo/trabajo/SegundoEje.java
@@ -5,20 +5,23 @@ import java.util.Scanner;
 public class SegundoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("escriba el numero");
-        int numero1=textoScanner.nextInt();
-        System.out.println("escriba el numero");
-        int numero2=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("escriba el numero");
+            int numero1=textoScanner.nextInt();
+            System.out.println("escriba el numero");
+            int numero2=textoScanner.nextInt();
         
-        if(numero1!=numero2){
-            int sum = numero1+numero2;
-            int mul = numero1*numero2;
-            System.out.println("la suma es "+sum);
-            System.out.println("la multiplicacion es "+mul);
-        }else{
-            System.out.println("los numeros son iguales");
+            if(numero1!=numero2){
+                int sum = numero1+numero2;
+                int mul = numero1*numero2;
+                System.out.println("la suma es "+sum);
+                System.out.println("la multiplicacion es "+mul);
+            }else{
+                System.out.println("los numeros son iguales");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
     }
     
