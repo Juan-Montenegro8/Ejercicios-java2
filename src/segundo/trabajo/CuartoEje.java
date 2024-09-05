1ad43cf8f44d80715c893caebafae58020341562
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-04 20:09:58 -0500

    second commit scanner

--- src/segundo/trabajo/CuartoEje.java
+++ src/segundo/trabajo/CuartoEje.java
@@ -5,27 +5,32 @@ import java.util.Scanner;
 public class CuartoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("Precio por hora");
-        double PrecioHora=textoScanner.nextDouble();
-        System.out.println("horas trabajada");
-        double HoraTrabjada=textoScanner.nextDouble();
-        System.out.println("horas extras");
-        double HorasExtra=textoScanner.nextDouble();
+        try (Scanner textoScanner=new Scanner(System.in)){
+            
+            System.out.println("Precio por hora");
+            double PrecioHora=textoScanner.nextDouble();
+            System.out.println("horas trabajada");
+            double HoraTrabjada=textoScanner.nextDouble();
+            System.out.println("horas extras");
+            double HorasExtra=textoScanner.nextDouble();
         
-        double ValorHora=PrecioHora*HoraTrabjada;
-        double ValorExtra=PrecioHora*HorasExtra;
-        double total=ValorHora+ValorExtra;
+            double ValorHora=PrecioHora*HoraTrabjada;
+            double ValorExtra=PrecioHora*HorasExtra;
+            double total=ValorHora+ValorExtra;
         
-        if(total<=500000){
-            double descuento=total*0.03;
-            double Pagar=total-descuento;
-            System.out.println("el salario es de "+Pagar);
-        }else{
-            double descuento=total*0.05;
-            double Pagar=total-descuento;
-            System.out.println("el salario es de "+Pagar);
+            if(total<=500000){
+                double descuento=total*0.03;
+                double Pagar=total-descuento;
+                System.out.println("el salario es de "+Pagar);
+            }else{
+                double descuento=total*0.05;
+                double Pagar=total-descuento;
+                System.out.println("el salario es de "+Pagar);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
         
     }
