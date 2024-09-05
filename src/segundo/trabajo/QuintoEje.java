4cb27c80acba9599bf27f460f566547b5073331c
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-04 20:11:23 -0500

    seconds commit scanner

--- src/segundo/trabajo/QuintoEje.java
+++ src/segundo/trabajo/QuintoEje.java
@@ -5,19 +5,23 @@ import java.util.Scanner;
 public class QuintoEje {
 
     public static void main(String[] args) {
-        Scanner textScanner= new Scanner(System.in);
         
-        System.out.println("ingrese un numero");
-        int numero1=textScanner.nextInt();
-        System.out.println("ingrese un numero");
-        int numero2=textScanner.nextInt();
+        try (Scanner textScanner= new Scanner(System.in)) {
+            
+            System.out.println("ingrese un numero");
+            int numero1=textScanner.nextInt();
+            System.out.println("ingrese un numero");
+            int numero2=textScanner.nextInt();
         
-        if(numero1==numero2){
-            int suma=numero1+numero2;
-            System.out.println("la suma es "+suma);
-        }else{
-            int resta=numero1-numero2;
-            System.out.println("la diferencia es "+resta);
+            if(numero1==numero2){
+                int suma=numero1+numero2;
+                System.out.println("la suma es "+suma);
+            }else{
+                int resta=numero1-numero2;
+                System.out.println("la diferencia es "+resta);
+            }    
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
     }
     
