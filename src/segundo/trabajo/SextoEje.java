d38072885203973198da9deec43198dedd6353ae
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-05 21:38:17 -0500

    Seconds commit scanner

--- src/segundo/trabajo/SextoEje.java
+++ src/segundo/trabajo/SextoEje.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 public class SextoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese numero");
-        double numero=textoScanner.nextDouble();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese numero");
+            double numero=textoScanner.nextDouble();
         
-        if(numero>=0){
-            double raiz=Math.sqrt(numero);
-            System.out.println("la raiz del numero es "+raiz);
-        }else{
-            System.out.println("no tiene raiz");
+            if(numero>=0){
+                double raiz=Math.sqrt(numero);
+                System.out.println("la raiz del numero es "+raiz);
+            }else{
+                System.out.println("no tiene raiz");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
     }
     
