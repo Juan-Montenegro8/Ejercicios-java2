028dd2de2ad1c51159ed9addd3627d01038c8e95
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-06 22:37:17 -0500

    third commit scanner

--- src/segundo/trabajo/OctavoEje.java
+++ src/segundo/trabajo/OctavoEje.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class OctavoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("escriba un caracter");
-        char caracter=textoScanner.next().charAt(0);
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("escriba un caracter");
+            char caracter=textoScanner.next().charAt(0);
         
-        if(Character.isUpperCase(caracter)){
-            System.out.println("Mayuscula");
-        }else{
-            System.out.println("Minuscula");
+            if(Character.isUpperCase(caracter)){
+                System.out.println("Mayuscula");
+            }else{
+                System.out.println("Minuscula");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
     }
     
