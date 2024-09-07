cdcd2da56747deffc2899ddd67611d20c8c1205f
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-06 22:38:26 -0500

    thirds commit scanner

--- src/segundo/trabajo/NovenoEje.java
+++ src/segundo/trabajo/NovenoEje.java
@@ -5,19 +5,22 @@ import java.util.Scanner;
 public class NovenoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese la nota final");
-        double nota=textoScanner.nextDouble();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("ingrese la nota final");
+            double nota=textoScanner.nextDouble();
         
-        if(nota>=0 && nota<=5){
-            if(nota>=3){
-                System.out.println("paso la materia");
+            if(nota>=0 && nota<=5){
+                if(nota>=3){
+                    System.out.println("paso la materia");
+                }else{
+                    System.out.println("perdio la metia");
+                }
             }else{
-                System.out.println("perdio la metia");
+                System.out.println("no esta entre los parametros");
             }
-        }else{
-            System.out.println("no esta entre los parametros");
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
     }
     
