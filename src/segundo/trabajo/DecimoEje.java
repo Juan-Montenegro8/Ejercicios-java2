96963cd38c5bc68e856f46d347a6847ce453c93f
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-09 20:00:02 -0500

    Thirds commit scanner

--- src/segundo/trabajo/DecimoEje.java
+++ src/segundo/trabajo/DecimoEje.java
@@ -5,21 +5,25 @@ import java.util.Scanner;
 public class DecimoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese un numero");
-        int numero1=textoScanner.nextInt();
-        System.out.println("ingrese un numero");
-        int numero2=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            
+            System.out.println("ingrese un numero");
+            int numero1=textoScanner.nextInt();
+            System.out.println("ingrese un numero");
+            int numero2=textoScanner.nextInt();
         
-        if(numero1<numero2){
-            System.out.println(numero2+" es mayor que "+numero1);
-        }else{
-            if(numero2<numero1){
-                System.out.println(numero1+" es mayor que "+numero2);
+            if(numero1<numero2){
+                System.out.println(numero2+" es mayor que "+numero1);
             }else{
-                System.out.println("los numero son iguales");
-            }
+                if(numero2<numero1){
+                    System.out.println(numero1+" es mayor que "+numero2);
+                }else{
+                    System.out.println("los numero son iguales");
+                }
+            }   
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
     }
     
