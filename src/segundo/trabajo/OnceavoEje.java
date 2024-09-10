062a1fe42ddb9be31acc6fe2b575cb48db5b1116
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-09 20:03:27 -0500

    Third commit scanner

--- src/segundo/trabajo/OnceavoEje.java
+++ src/segundo/trabajo/OnceavoEje.java
@@ -5,47 +5,51 @@ import java.util.Scanner;
 public class OnceavoEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("si el triangulo es rectangulo digite 1");
-        System.out.println("sino digite 2");
-        int rectangulo=textoScanner.nextInt();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            
+            System.out.println("si el triangulo es rectangulo digite 1");
+            System.out.println("sino digite 2");
+            int rectangulo=textoScanner.nextInt();
         
-        switch (rectangulo) {
-            case 1:                
-                System.out.println("ingrese base");
-                double base1=textoScanner.nextDouble();
-                System.out.println("ingrese altura");
-                double altura1=textoScanner.nextDouble();
-                if(base1>0 && altura1>0){
-                    double area1 =(base1*altura1)/2;
-                    System.out.println("el area del triangulo "+area1);
-                }else{
-                    System.out.println("no pueden ir numeros negativos o el 0");
-                }
-                break;
-            case 2:
-                System.out.println("ingrese base");
-                double base2=textoScanner.nextDouble();
-                System.out.println("ingrese cateto1");
-                double cateto1=textoScanner.nextDouble();
-                System.out.println("ingrese cateto2");
-                double cateto2=textoScanner.nextDouble();
+            switch (rectangulo) {
+                case 1:                
+                    System.out.println("ingrese base");
+                    double base1=textoScanner.nextDouble();
+                    System.out.println("ingrese altura");
+                    double altura1=textoScanner.nextDouble();
+                    if(base1>0 && altura1>0){
+                        double area1 =(base1*altura1)/2;
+                        System.out.println("el area del triangulo "+area1);
+                    }else{
+                        System.out.println("no pueden ir numeros negativos o el 0");
+                    }
+                    break;
+                case 2:
+                    System.out.println("ingrese base");
+                    double base2=textoScanner.nextDouble();
+                    System.out.println("ingrese cateto1");
+                    double cateto1=textoScanner.nextDouble();
+                    System.out.println("ingrese cateto2");
+                    double cateto2=textoScanner.nextDouble();
                 
-                if(base2>0 && cateto1>0 && cateto2>0){
-                    double semiperimetro =(cateto1+base2+cateto2)/2;
-                    double raiz=(semiperimetro*(semiperimetro-cateto1)*(semiperimetro-base2)*(semiperimetro-cateto2));
-                    //Cálculo del área usando la fórmula de Herón
-                    double area2 = Math.sqrt(raiz);
+                    if(base2>0 && cateto1>0 && cateto2>0){
+                        double semiperimetro =(cateto1+base2+cateto2)/2;
+                        double raiz=(semiperimetro*(semiperimetro-cateto1)*(semiperimetro-base2)*(semiperimetro-cateto2));
+                        //Cálculo del área usando la fórmula de Herón
+                        double area2 = Math.sqrt(raiz);
                 
-                    System.out.println("el area del triangulo "+area2);
-                }else{
-                    System.out.println("no pueden ir numeros negativos o el 0");
-                }
-            default:
-                System.out.println("dato invalido");;
-        }    
+                        System.out.println("el area del triangulo "+area2);
+                    }else{
+                        System.out.println("no pueden ir numeros negativos o el 0");
+                    }
+                default:
+                    System.out.println("dato invalido");;
+            }    
         
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
+        }
     }
     
 }
