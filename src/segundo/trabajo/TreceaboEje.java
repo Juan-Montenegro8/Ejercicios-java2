40133dbb4449d73945063c7385e5301e2b0ac31d
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-10 20:16:02 -0500

    Fourths commit scanner

--- src/segundo/trabajo/TreceaboEje.java
+++ src/segundo/trabajo/TreceaboEje.java
@@ -5,31 +5,34 @@ import java.util.Scanner;
 public class TreceaboEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("solucion de AX+B=0");
-        System.out.println("ingrese valor de A");
-        double ValorA=textoScanner.nextDouble();
-        System.out.println("ingrese valor de B");
-        double ValorB=textoScanner.nextDouble();
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("solucion de AX+B=0");
+            System.out.println("ingrese valor de A");
+            double ValorA=textoScanner.nextDouble();
+            System.out.println("ingrese valor de B");
+            double ValorB=textoScanner.nextDouble();
         
-        if(ValorA!=0){
-            if((ValorA>0 && ValorB>=0)){
-                double resultado=-(ValorB/ValorA);
-                System.out.println("el resultado es "+resultado);
-            }else{
-                if((ValorA<0 && ValorB>=0) || (ValorA>0 && ValorB<0)){
-                    double resultado=-1*(ValorB/ValorA);
+            if(ValorA!=0){
+                if((ValorA>0 && ValorB>=0)){
+                    double resultado=-(ValorB/ValorA);
                     System.out.println("el resultado es "+resultado);
                 }else{
-                    if(ValorA<0 && ValorB<0){
-                        double resultado=(ValorB/ValorA);
+                    if((ValorA<0 && ValorB>=0) || (ValorA>0 && ValorB<0)){
+                        double resultado=-1*(ValorB/ValorA);
                         System.out.println("el resultado es "+resultado);
+                    }else{
+                        if(ValorA<0 && ValorB<0){
+                            double resultado=(ValorB/ValorA);
+                            System.out.println("el resultado es "+resultado);
+                        }
                     }
-                }
-            }    
-        }else{
-            System.out.println("la letra A no puede ir negativa");
+                }    
+            }else{
+                System.out.println("la letra A no puede ir negativa");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error:"+e.getMessage());
         }
                 
     }
