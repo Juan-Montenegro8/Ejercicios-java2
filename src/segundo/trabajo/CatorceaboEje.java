10c3b07a37d1299b892d57619a22e925543aa129
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-11 20:29:58 -0500

    fourths commit scanner

--- src/segundo/trabajo/CatorceaboEje.java
+++ src/segundo/trabajo/CatorceaboEje.java
@@ -5,33 +5,35 @@ import java.util.Scanner;
 public class CatorceaboEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
+         
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            System.out.println("resolver ax^2+bx+c=0");
+            System.out.println("ingrese valor de a");
+            double ValorA=textoScanner.nextDouble();
+            System.out.println("ingrese valor de b");
+            double ValorB=textoScanner.nextDouble();
+            System.out.println("ingrese valor de c");
+            double ValorC=textoScanner.nextDouble();
         
-        System.out.println("resolver ax^2+bx+c=0");
-        System.out.println("ingrese valor de a");
-        double ValorA=textoScanner.nextDouble();
-        System.out.println("ingrese valor de b");
-        double ValorB=textoScanner.nextDouble();
-        System.out.println("ingrese valor de c");
-        double ValorC=textoScanner.nextDouble();
-        
-        if(ValorA!=0 && ValorB!=0){
-            double potencia=Math.pow(ValorB, 2);
-            double Raiz=potencia-4*ValorA*ValorC;
-            if(Raiz>=0){
-                double ResultadoRaiz=Math.sqrt(Raiz);
-                double resultado1=(-ValorB+ResultadoRaiz)/2*ValorA;
-                double resultado2=(-ValorB-ResultadoRaiz)/2*ValorA;
-                System.out.println("resultado 1= "+resultado1);
-                System.out.println("resultado 2= "+resultado2);     
-            }else{
-                System.out.println("no se le puede sacar raiz cuadrada numero negativo");
-            }
+            if(ValorA!=0 && ValorB!=0){
+                double potencia=Math.pow(ValorB, 2);
+                double Raiz=potencia-4*ValorA*ValorC;
+                if(Raiz>=0){
+                    double ResultadoRaiz=Math.sqrt(Raiz);
+                    double resultado1=(-ValorB+ResultadoRaiz)/2*ValorA;
+                    double resultado2=(-ValorB-ResultadoRaiz)/2*ValorA;
+                    System.out.println("resultado 1= "+resultado1);
+                    System.out.println("resultado 2= "+resultado2);     
+                }else{
+                    System.out.println("no se le puede sacar raiz cuadrada numero negativo");
+                }
                         
-        }else{
-            System.out.println("el valor A y B no puden equivaler 0");
+            }else{
+                System.out.println("el valor A y B no puden equivaler 0");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
-                       
     }
     
 }
---
+++ src/segundo/trabajo/Ciclo.java
@@ -0,0 +1,15 @@
+package segundo.trabajo;
+
+
+
+public class jj {
+
+    public static void main(String[] args) {
+        
+        for(int i=1;i<100;i++){
+            System.out.println(i);
+        }
+                
+    }
+    
+}
