684ad7bc009456b1e769e7044357c48e7c95dc23
Juan-Montenegro8 <jj.monte2002@gmail.com>
2024-09-12 22:59:36 -0500

    Fifth commit scanner

--- src/segundo/trabajo/QuinceaboEje.java
+++ src/segundo/trabajo/QuinceaboEje.java
@@ -5,41 +5,45 @@ import java.util.Scanner;
 public class QuinceaboEje {
 
     public static void main(String[] args) {
-        Scanner textoScanner=new Scanner(System.in);
         
-        System.out.println("ingrese un numero");
-        int numero1=textoScanner.nextInt();
-        System.out.println("ingrese un numero");
-        int numero2=textoScanner.nextInt();
-        System.out.println("ingrese un numero");
-        int numero3=textoScanner.nextInt();
-        
-        if(numero1!=numero2 && numero2!=numero3){
-            if(numero1>numero2 && numero1>numero3 && numero2>numero3){
-                System.out.println("de mayor a menor"+numero1+" - "+numero2+" - "+numero3);
-            }else{
-                if(numero1>numero2 && numero1>numero3 && numero3>numero2){
-                    System.out.println("de mayor a menor"+numero1+" - "+numero3+" - "+numero2);
+        try (Scanner textoScanner=new Scanner(System.in)) {
+            
+            System.out.println("ingrese un numero");
+            int numero1=textoScanner.nextInt();
+            System.out.println("ingrese un numero");
+            int numero2=textoScanner.nextInt();
+            System.out.println("ingrese un numero");
+            int numero3=textoScanner.nextInt();
+            
+            if(numero1!=numero2 && numero2!=numero3){
+                if(numero1>numero2 && numero1>numero3 && numero2>numero3){
+                    System.out.println("de mayor a menor"+numero1+" - "+numero2+" - "+numero3);
                 }else{
-                    if(numero2>numero1 && numero2>numero3 && numero1>numero3){
-                        System.out.println("de mayor a menor"+numero2+" - "+numero1+" - "+numero3);
+                    if(numero1>numero2 && numero1>numero3 && numero3>numero2){
+                        System.out.println("de mayor a menor"+numero1+" - "+numero3+" - "+numero2);
                     }else{
-                        if(numero2>numero1 && numero2>numero3 && numero3>numero1){
-                            System.out.println("de mayor a menor"+numero2+" - "+numero3+" - "+numero1);
+                        if(numero2>numero1 && numero2>numero3 && numero1>numero3){
+                            System.out.println("de mayor a menor"+numero2+" - "+numero1+" - "+numero3);
                         }else{
-                            if(numero3>numero1 && numero3>numero2 && numero1>numero2){
-                                System.out.println("de mayor a menor"+numero3+" - "+numero1+" - "+numero2);
+                            if(numero2>numero1 && numero2>numero3 && numero3>numero1){
+                                System.out.println("de mayor a menor"+numero2+" - "+numero3+" - "+numero1);
                             }else{
-                                if(numero3>numero1 && numero3>numero2 && numero2>numero1){
-                                    System.out.println("de mayor a menor"+numero3+" - "+numero2+" - "+numero1);
+                                if(numero3>numero1 && numero3>numero2 && numero1>numero2){
+                                    System.out.println("de mayor a menor"+numero3+" - "+numero1+" - "+numero2);
+                                }else{
+                                    if(numero3>numero1 && numero3>numero2 && numero2>numero1){
+                                        System.out.println("de mayor a menor"+numero3+" - "+numero2+" - "+numero1);
+                                    }
                                 }
                             }
                         }
                     }
-                }
-            }    
-        }else{
-            System.out.println("hay numeros iguales");
+                }    
+            }else{
+                System.out.println("hay numeros iguales");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error: "+e.getMessage());
         }
                 
     }
